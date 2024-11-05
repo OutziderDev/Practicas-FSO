10317137eff5d4f4406b1ab50a848f5623692f06
Luis <luisbarrios0110@gmail.com>
2024-11-05 13:26:44 -0500

    trabajando con Delete.API para borrar informacion por las solicitudes HTTP

--- PracticesExpres/index.js
+++ PracticesExpres/index.js
@@ -5,29 +5,29 @@ const id = 0
 
 let notes = [
       {
-        "id": "1",
-        "content": "HTML is not Easy",
-        "important": false
+        id: 1,
+        content: "HTML is not Easy",
+        important: false
       },
       {
-        "id": "2",
-        "content": "El navegador puede ejecutar solo JavaScript",
-        "important": false
+        id: 2,
+        content: "El navegador puede ejecutar solo JavaScript",
+        important: false
       },
       {
-        "id": "3",
-        "content": "El metodo GET y POST son los mas importantes del protocolo HTTP.",
-        "important": false
+        id: 3,
+        content: "El metodo GET y POST son los mas importantes del protocolo HTTP.",
+        important: false
       },
       {
-        "id": "4",
-        "content": "Maria la de paco",
-        "important": false
+        id: 4,
+        content: "Maria la de paco",
+        important: false
       },
       {
-        "id": "5",
-        "content": "La pizza es vida",
-        "important": true
+        id: 5,
+        content: "La pizza es vida",
+        important: true
       }
     ]
   
@@ -40,13 +40,20 @@ let notes = [
  })
 
  app.get(`/api/note/:id`,(req,res)=>{
-    const id = req.params.id;
-    const note = notes[id-1]
+    const id = Number(req.params.id);
+    const note = notes.find(note =>  note.id === id)
     if (note) {
         res.json(note)
     }else{
         res.status(404).json({error:'no encontrada'})
     }
+ })
+
+ app.delete(`/api/notes/:id`,(req,res)=>{
+    const id = Number(req.params.id)
+    const notesDel = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
  })
 
 
