6d28d7b10ef4a91c11506d01bc6e0ad2454053bd
Luis <luisbarrios0110@gmail.com>
2024-11-06 17:06:34 -0500

    Practicando con Express desarrollando la API con POST, DELETE,GET --falta PATCH o PUT

--- PracticesExpres/index.js
+++ PracticesExpres/index.js
@@ -1,35 +1,34 @@
 const express = require('express');
 const app = express();
 const PORT = 3001
-const id = 0
 
 let notes = [
-      {
-        id: 1,
-        content: "HTML is not Easy",
-        important: false
-      },
-      {
-        id: 2,
-        content: "El navegador puede ejecutar solo JavaScript",
-        important: false
-      },
-      {
-        id: 3,
-        content: "El metodo GET y POST son los mas importantes del protocolo HTTP.",
-        important: false
-      },
-      {
-        id: 4,
-        content: "Maria la de paco",
-        important: false
-      },
-      {
-        id: 5,
-        content: "La pizza es vida",
-        important: true
-      }
-    ]
+  {
+    id: 1,
+    content: "HTML is not Easy",
+    important: false
+  },
+  {
+    id: 2,
+    content: "El navegador puede ejecutar solo JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "El metodo GET y POST son los mas importantes del protocolo HTTP.",
+    important: false
+  },
+  {
+    id: 4,
+    content: "Maria la de paco",
+    important: false
+  },
+  {
+    id: 5,
+    content: "La pizza es vida",
+    important: true
+  }
+]
   
  app.get('/',(req,res)=>{
     res.send(`<h1>Hello Words!: ${notes[0].content}</h1>`)
@@ -37,6 +36,7 @@ let notes = [
 
  app.get('/api/notes',(req,res)=>{
     res.json(notes)
+    //console.log(req);
  })
 
  app.get(`/api/note/:id`,(req,res)=>{
@@ -49,6 +49,7 @@ let notes = [
     }
  })
 
+ app.use(express.json())
  app.delete(`/api/notes/:id`,(req,res)=>{
     const id = Number(req.params.id)
     notes = notes.filter(note => note.id !== id)
@@ -56,7 +57,31 @@ let notes = [
     res.status(204).end()
  })
 
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(404).json({
+      error : "falto el contenido de la nota"
+    })
+  }
+  const note = {
+    id: generateId(),
+    content: body.content,
+    importat: Boolean(body.important) || false
+  }
+  notes = notes.concat(note);
+  response.json(note);
+})
+
+//Funciones de apoyo
+const generateId = () =>{
+  const MaxID = notes.length >0 ? Math.max(...notes.map(max => max.id)) : 0
+  return MaxID + 1
+}
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT} --`);
+    console.log('For exit of Server press CTRL + C');
+    
 });
