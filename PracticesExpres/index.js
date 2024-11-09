900f186f8948e8cb6de9cd01f4284ce5e11b6194
Luis <luisbarrios0110@gmail.com>
2024-11-09 12:10:24 -0500

    Reparando la ruta para escuchar los puertos

--- PracticesExpres/index.js
+++ PracticesExpres/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
-const PORT = 3001
+const PORT = process.env.PORT || 4000
 
 let notes = [
   {
