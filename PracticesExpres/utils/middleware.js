cfdb7eb4ecd61aaa4e0f5cc28a71070ad4a21097
Luis <luisbarrios0110@gmail.com>
2024-11-26 23:02:13 -0500

    Estableciendo la Estructura final del proyecto y desglosando los middleware separados en utils/middlewares, la aplicacion ahora se genera desde el fichero App.js

---
+++ PracticesExpres/utils/middleware.js
@@ -0,0 +1,27 @@
+const Logger = require('./logger')
+
+const requestLogger = (request, next) => {
+  Logger.info('Method', request.method)
+  Logger.info('Path', request.path)
+  Logger.info('Body',request.body)
+  Logger.info('---')
+  next()
+}
+
+const unknownEndpoint = ( response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  Logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(404).send({ error: 'malformatted Id' })
+  }else if (error.name === 'ValidationError'){
+    return response.status(404).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = { requestLogger, unknownEndpoint, errorHandler }
