c0cd9829a2cd58609af80265323705a634b4cccb
Luis <luisbarrios0110@gmail.com>
2024-12-19 17:35:56 -0500

    Agregando estilos a la pagina de notas y agregando el formulario de login

--- PracticaGeneral/eslint.config.js
+++ PracticaGeneral/eslint.config.js
@@ -33,6 +33,15 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      '@stylistic/js/indent': [      'warn',      2    ],
+      '@stylistic/js/linebreak-style': [      'error',      'windows'    ],
+      '@stylistic/js/quotes': [      'error',      'single'    ],
+      '@stylistic/js/semi': [      'error',      'never'    ],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [      'error', 'always'    ],
+      'arrow-spacing': [      'error', { 'before': true, 'after': true },    ],
+      'no-console': 'off'
     },
   },
 ]
--- PracticaGeneral/src/App.jsx
+++ PracticaGeneral/src/App.jsx
@@ -35,12 +35,12 @@ const App = () =>{
       .then(UpdateNote => {
         setNote(note.map(noteForServerSet => noteForServerSet.id !== id ? noteForServerSet : UpdateNote))
       })
-      .catch(error => {
+      .catch(() => {
         setErrorMesage(`Note '${changeNote.contenido}' was already removed from server`)
         setTimeout(() => {
           setErrorMesage(null)
         }, 5000);
-        setNote(notes.filter(n => n.id !== id))
+        setNote(note.filter(n => n.id !== id)) //verificar por que tenia notes.filter
       })
   };
   const notesToShow = showAll ? note : note.filter(note => note.important)
@@ -48,28 +48,51 @@ const App = () =>{
   
   return(
     <>
-      <h1>Notas:</h1>
-      <Notification message={errorMesage}/>
-      <ul>
-        {
-          notesToShow.map((notex) => 
-            <Note key={notex.id} contenido={notex} toggleImportance={()=>toggleImportanceOf(notex.id)}/>
-          )
-        }        
-      </ul>
-      <form onSubmit={addNote} id='form'>
-        <input type="text"
-               id='text'
-               value={newNote}
-               onChange={onChangeHandler}
-        />
-        <br />
-        <button style={{marginTop:'5px'}} type='submit' id='but'>guardar</button>
-      </form>
-      <br />
-      <button onClick={()=>setShowAll(!showAll)}>
-        Ver {showAll ? 'importantes' : 'todas'}
-      </button>
+      <div id='container'>
+        <h1>Registro de Notas:</h1>
+        <Notification message={errorMesage}/>
+        <div className='grid'>
+          <section> {/* Contenedor para Login */}
+            <h3>Login</h3>
+            <form id='form-login'>
+              <label>Usuario:</label>
+              <input type='text' id='user' name='user' placeholder='pepe'/>
+              <br/>
+              <label>Contraseña:</label>
+              <input type='password' id='pass' name='pass' placeholder='pass456'/>
+            </form> 
+          </section>
+
+          <section> {/*Contenedor para Notas */}
+            <h3>Notas:</h3>
+            <ul>
+              {
+                notesToShow.map((notex) => 
+                  <Note key={notex.id} contenido={notex} toggleImportance={()=>toggleImportanceOf(notex.id)}/>
+                )
+              }        
+            </ul>
+
+            <form onSubmit={addNote} id='form-save'>
+              <input type="text"
+                    id='text'
+                    value={newNote}
+                    placeholder='Write your Note: Html is easy'
+                    onChange={onChangeHandler}
+              />
+              <br />
+              <button style={{marginTop:'5px'}} type='submit' id='but'>guardar</button>
+            </form>
+
+            
+            <button id='btn' onClick={()=>setShowAll(!showAll)}>
+              Ver {showAll ? 'importantes' : 'todas'}
+            </button>
+            
+          </section>
+        </div>
+      </div>
+
       {//console.log(note)
       }
     </>
