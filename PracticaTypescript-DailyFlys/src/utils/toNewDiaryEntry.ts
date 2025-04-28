09515369a4265564e5dff9eb14c4fbd32773541f
Luis <luisbarrios0110@gmail.com>
2025-04-28 17:21:16 -0500

    Guardias de tipo con Zod para validaciones de typescrip

--- PracticaTypescript-DailyFlys/src/utils/toNewDiaryEntry.ts
+++ PracticaTypescript-DailyFlys/src/utils/toNewDiaryEntry.ts
@@ -1,5 +1,12 @@
 import { NewDiaryEntry } from "../types/DairyEntryType";
+import { z } from "zod";
 
+const NewDiaryEntrySchema = z.object({
+  date: z.string(),
+  weather: z.enum(["sunny", "rainy", "cloudy", "stormy", "windy"]),
+  visibility: z.enum([    "great",    "good",    "ok",    "poor",  ]),
+  comment: z.string().optional()
+})
 
 export const toNewDiaryEntry = ( data: unknown ): NewDiaryEntry => {
   return NewDiaryEntrySchema.parse(data)
