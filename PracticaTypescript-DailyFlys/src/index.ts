4640b3f023d453602cbf345421aa56fa6b6770a8
Luis <luisbarrios0110@gmail.com>
2025-05-02 23:21:23 -0500

    Aplicando las Cors para poder extraer los datos en el Frontend en el ejercicio 9.16

--- PracticaTypescript-DailyFlys/src/index.ts
+++ PracticaTypescript-DailyFlys/src/index.ts
@@ -1,9 +1,11 @@
-import express from 'express';
 import diaryRouter from './routes/diariesRoute';
+import express from 'express';
+import cors from 'cors'
 
 const PORT = 3001;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/diaries', diaryRouter);
 
